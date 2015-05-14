149bc849f959e2b553782ebfb342018d365eda16
Bloodskate <prasannamishra50@gmail.com>
2015-05-14 16:52:09 +0545

    aa

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var elixir = require('laravel-elixir');
 
+var paths = {
+	'bootstrap': './resources/assets/bower/bootstrap-sass-official/assets/'
+}
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -13,4 +17,14 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.less('app.less');
+    mix.scripts([
+    '../assets/bower/jquery/dist/jquery.js',
+    '../assets/bower/bootstrap/dist/js/bootstrap.js'
+], 'public/js/vendor.js');
+     mix.sass("style.scss", 'public/css/', {includePaths: [paths.bootstrap + 'stylesheets/']})
+	      .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
+	      .scripts([
+	        paths.jquery + "dist/jquery.js",
+	        paths.bootstrap + "javascripts/bootstrap.js"
+	      ], './', 'public/js/app.js');
 });
